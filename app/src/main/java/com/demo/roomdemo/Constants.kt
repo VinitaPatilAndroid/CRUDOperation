cf90b637aa7d87cf5a3c7164faee46bcf74e6564
vinita <vinitavp@ix.lentra.ai>
2023-01-11 14:46:47 +0530

    CRUD operation with mvvm,corotuines,lambda function, constants file

---
+++ app/src/main/java/com/demo/roomdemo/Constants.kt
@@ -0,0 +1,13 @@
+package com.demo.roomdemo
+
+class Constants {
+    companion object{
+
+        val YES = "Yes"
+        val NO = "No"
+        val ARE_YOU_SURE_WANT_TO_DELETE = "Are you sure want to Delete?"
+        val SAVE = "Save"
+        val UPDATE = "Update"
+
+    }
+}
